f2645d8aa4a9d4b631ac14265d63e9e422ecc58a
Diego Chavez <ingdiegochavez18@gmail.com>
2021-09-12 14:00:48 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const http = require('./app');
 
-const port = 4001;
+const port = 3000;
 
 http.listen(port, () => {
-    console.log('server is running on port', port);
+//     console.log('server is running on port', port);
 });
